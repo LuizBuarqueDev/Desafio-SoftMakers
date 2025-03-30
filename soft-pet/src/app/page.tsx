968b686870182b9875c104c8539026ac1389137b
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-03-30 18:08:37 -0300

    Ajustes nas cores dos botões

--- soft-pet/src/app/page.tsx
+++ soft-pet/src/app/page.tsx
@@ -4,12 +4,23 @@ import { SearchBar } from "@/components/search_bar/SearchBar";
 export default function Home() {
   return (
     <main>
-      <Header/>
-      <div>
-      <SearchBar/>
-      <button className="bg-[var(--gray)] text-[var(--light)] absolute right-1.5 transform  focus:ring-4 focus:ring-black font-bold rounded-lg text-sm p-2 px-3">
-      cadastrar
-      </button>
+      <Header />
+      <div className="flex justify-between relative w-full">
+        <div className="flex flex-grow">
+          <SearchBar />
+        </div>
+
+        <button
+          className="text-[var(--light)] transform focus:ring-2 focus:[var(--light)] font-bold rounded-lg flex items-center gap-2 text-sm p-2 px-3 mx-3"
+          style={{ background: "var(--gradient_2)" }}
+        >
+          <svg className="w-[20px] h-[20px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+
+          Cadastrar
+        </button>
+
       </div>
 
     </main>
--- soft-pet/src/components/search_bar/SearchBar.tsx
+++ soft-pet/src/components/search_bar/SearchBar.tsx
@@ -9,27 +9,27 @@ export function SearchBar() {
   };
 
   return (
-<form onSubmit={handleSubmit} className="pl-8 mx-auto">
-  <div className="relative">
-    <div className="bg-[var(--gray)] absolute inset-y-0 flex items-center p-3 rounded-l-lg">
-      <svg className="w-4 h-4 text-[var(--color_1)]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
-      </svg>
-    </div>
-    <input
-      type="search"
-      className="block w-full p-3 ps-10 text-[var(--light)] border-3 border-[var(--gray)] rounded-lg focus:border-[var(--gray)] focus:ring-0"
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-      required
-    />
-    <button
-      type="submit"
-      className="bg-[var(--gray)] text-[var(--light)] absolute top-1/2 right-1.5 transform -translate-y-1/2 focus:ring-2 focus:ring-[var(--light)] font-bold rounded-lg text-sm p-2 px-3"
-    >
-      Pesquisar
-    </button>
-  </div>
-</form>
+    <form onSubmit={handleSubmit} className="flex w-full ml-12">
+      <div className="relative w-full">
+        <div className="bg-[var(--gray)] absolute inset-y-0 flex items-center p-3 rounded-l-lg">
+          <svg className="w-4 h-4 text-[var(--color_1)]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
+          </svg>
+        </div>
+        <input
+          type="search"
+          className="block w-full p-3 ps-10 text-[var(--light)] border-3 border-[var(--gray)] rounded-lg focus:border-[var(--gray)] focus:ring-0"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          required
+        />
+        <button
+          type="submit"
+          className="bg-[var(--gray)] text-[var(--light)] absolute top-1/2 right-1.5 transform -translate-y-1/2 focus:ring-2 focus:ring-[var(--light)] font-bold rounded-lg text-sm p-2 px-3"
+        >
+          Pesquisar
+        </button>
+      </div>
+    </form>
   );
 }
